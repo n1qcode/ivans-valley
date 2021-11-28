ac248e9718a5a1bae0c7c54df301018ac884b6e1
Nick <89103347+n1qcode@users.noreply.github.com>
2021-11-28 08:58:07 +0300

    Add files via upload

---
+++ js/m-menu.js
@@ -0,0 +1,27 @@
+const triggers = document.querySelectorAll('.menu-trigger'), activeClass = 'active';
+
+const menuButton = document.querySelector('.menu-trigger');
+const menuFull = document.querySelectorAll('.m-menu-main')
+
+triggers.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        e.currentTarget.classList.toggle(activeClass);
+    });
+});
+
+menuButton.addEventListener('click', (event) => {
+    menuFull.forEach((event) => {
+    event.classList.toggle('is-open');
+})
+})
+
+const menuBlock = document.querySelector('.menu-trigger');
+const menuBlockFull = document.querySelector('.m-menu-top-sets')
+const menuBlockFull2 = document.querySelector('.m-menu-main-block')
+const menuLogoHide = document.querySelector('.m-menu-logo')
+
+menuBlock.addEventListener('click', (event) => {
+    menuBlockFull.classList.toggle('is-open2');
+    menuBlockFull2.classList.toggle('is-open5');
+    menuLogoHide.classList.toggle('is-open3');
+});
--- js/qrcodes.js
+++ js/qrcodes.js
@@ -1,28 +1,79 @@
 'use strict'
+// mobile
+
+const menuBlockFullqr = document.querySelector('.m-menu-top-sets')
+const menuBlockFullqr2 = document.querySelector('.m-menu-main-block')
 
 const qrIcon1 = document.querySelector('.menu-item-qr-code1');
 const qrIcon2 = document.querySelector('.menu-item-qr-code2');
 const qrIcon3 = document.querySelector('.menu-item-qr-code3');
+
+// mobile
 const qrCode1 = document.querySelector('.qr-code1');
 const qrCode2 = document.querySelector('.qr-code2');
 const qrCode3 = document.querySelector('.qr-code3');
 
-qrIcon1.addEventListener('mouseover', (event) => {
+//full
+const qrIcon11 = document.querySelector('.menu-item-qr-code1-second');
+const qrIcon12 = document.querySelector('.menu-item-qr-code2-second');
+const qrIcon13 = document.querySelector('.menu-item-qr-code3-second');
+
+//full
+const qrCode11 = document.querySelector('.qr-code1-second');
+const qrCode12 = document.querySelector('.qr-code2-second');
+const qrCode13 = document.querySelector('.qr-code3-second');
+
+// mobile
+qrIcon1.addEventListener('click', (event) => {
    qrCode1.classList.remove('hidden-qr1');
+   menuBlockFullqr.classList.add('is-open4');
+   menuBlockFullqr2.classList.add('is-open6');
 });
-qrIcon2.addEventListener('mouseover', (event) => {
+qrIcon2.addEventListener('click', (event) => {
    qrCode2.classList.remove('hidden-qr2');
+   menuBlockFullqr.classList.add('is-open4');
+   menuBlockFullqr2.classList.add('is-open6');
 });
-qrIcon3.addEventListener('mouseover', (event) => {
+qrIcon3.addEventListener('click', (event) => {
    qrCode3.classList.remove('hidden-qr3');
+   menuBlockFullqr.classList.add('is-open4');
+   menuBlockFullqr2.classList.add('is-open6');
 });
 
 qrIcon1.addEventListener('mouseout', (event) => {
    qrCode1.classList.add('hidden-qr1');
+   menuBlockFullqr.classList.remove('is-open4');
+   menuBlockFullqr2.classList.remove('is-open6');
 });
 qrIcon2.addEventListener('mouseout', (event) => {
    qrCode2.classList.add('hidden-qr2');
+   menuBlockFullqr.classList.remove('is-open4');
+   menuBlockFullqr2.classList.remove('is-open6');
 });
 qrIcon3.addEventListener('mouseout', (event) => {
    qrCode3.classList.add('hidden-qr3');
+   menuBlockFullqr.classList.remove('is-open4');
+   menuBlockFullqr2.classList.remove('is-open6');
+});
+
+
+//full
+qrIcon11.addEventListener('mouseover', (event) => {
+   qrCode11.classList.remove('hidden-qr1');
+});
+qrIcon12.addEventListener('mouseover', (event) => {
+   qrCode12.classList.remove('hidden-qr2');
+});
+qrIcon13.addEventListener('mouseover', (event) => {
+   qrCode13.classList.remove('hidden-qr3');
+});
+
+qrIcon11.addEventListener('mouseout', (event) => {
+   qrCode11.classList.add('hidden-qr1');
+});
+qrIcon12.addEventListener('mouseout', (event) => {
+   qrCode12.classList.add('hidden-qr2');
+});
+qrIcon13.addEventListener('mouseout', (event) => {
+   qrCode13.classList.add('hidden-qr3');
 });
